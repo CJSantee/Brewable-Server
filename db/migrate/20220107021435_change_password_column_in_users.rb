ec6a975a910b2a05f55e7d8dc72b7a02167f0474
CJSantee <santeedev@gmail.com>
2022-08-09 17:57:05 -0500

    DELETE ALL

--- db/migrate/20220107021435_change_password_column_in_users.rb
+++
@@ -1,7 +0,0 @@
-class ChangePasswordColumnInUsers < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :users, :encrypted_password
-    remove_column :users, :salt
-    add_column :users, :password_digest, :string
-  end
-end
